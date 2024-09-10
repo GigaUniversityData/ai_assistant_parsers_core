4f98ebd936a10a7380bce3886eb3eca8c924adf9
LEv145 <you@example.com>
2024-09-10 16:37:43 +0200

    Добавлено описание и улучшена типизация

--- src/ai_assistant_parsers_core/parsers/utils/clean_blocks.py
+++ src/ai_assistant_parsers_core/parsers/utils/clean_blocks.py
@@ -1,24 +1,53 @@
+"""Утилиты для очистки HTML-блоков."""
+
 from __future__ import annotations
+
+import typing as t
 
 from bs4 import BeautifulSoup, Tag, NavigableString
 
 
-def clean_one_by_find(soup: BeautifulSoup, args: dict):
+# TODO?: Можно перейти от словаря к `**kwargs`
+def clean_one_by_find(soup: BeautifulSoup | Tag, args: dict[str, t.Any]):
+    """Очищает один HTML-блок по аргументам через `soup.find`.
+
+    Args:
+        soup (BeautifulSoup | Tag): Объект beautiful soup.
+        args (dict[str, t.Any]): Словарь аргументов для `soup.find`.
+    """
     element = soup.find(**args)
     _clean_one_element(element)
 
 
-def clean_one_by_select(soup: BeautifulSoup, selector: str):
+def clean_one_by_select(soup: BeautifulSoup | Tag, selector: str):
+    """Очищает один HTML-блок по селектору через `soup.select_one`.
+
+    Args:
+        soup (BeautifulSoup | Tag): Объект beautiful soup.
+        selector (str): CSS-селектор для `soup.select_one`.
+    """
     element = soup.select_one(selector)
     _clean_one_element(element)
 
 
-def clean_all_by_find(soup: BeautifulSoup, args: dict) -> None:
+def clean_all_by_find(soup: BeautifulSoup | Tag, args: dict[str, t.Any]) -> None:
+    """Очищает все HTML-блоки по аргументам через `soup.find_all`.
+
+    Args:
+        soup (BeautifulSoup | Tag): Объект beautiful soup.
+        args (dict[str, t.Any]): Словарь аргументов для `soup.find_all`.
+    """
     for element in soup.find_all(**args):
         _clean_one_element(element)
 
 
-def clean_all_by_select(soup: BeautifulSoup, selector: str) -> None:
+def clean_all_by_select(soup: BeautifulSoup | Tag, selector: str) -> None:
+    """Очищает все HTML-блоки по селектору через `soup.select`.
+
+    Args:
+        soup (BeautifulSoup | Tag): Объект beautiful soup.
+        selector (str): CSS-селектор для `soup.select`.
+    """
     for element in soup.select(selector):
         _clean_one_element(element)
 
--- src/ai_assistant_parsers_core/parsers/utils/restructure_blocks.py
+++ src/ai_assistant_parsers_core/parsers/utils/restructure_blocks.py
@@ -1,24 +1,49 @@
+"""
+Утилиты для изменение структуры HTML-кода. 
+Как правило, нужны для улучшения читаемости HTML-кода.
+"""
+
 from __future__ import annotations
 
 from bs4 import BeautifulSoup, Tag
 
 
-def rename_all_by_select(soup: BeautifulSoup, selector: str, replace_name: str) -> None:
+def rename_all_by_select(soup: BeautifulSoup | Tag, selector: str, replace_name: str) -> None:
+    """Меняет имя всем HTML-тегам на заданное по селектору для `soup.select`.
+
+    Args:
+        soup (BeautifulSoup | Tag): Объект beautiful soup.
+        selector (str): CSS-селектор для `soup.select`
+        replace_name (str): Имя для замены.
+    """
     for tag in soup.select(selector):
-        _raname_element(tag, replace_name=replace_name)
+        _rename_element(tag, replace_name=replace_name)
 
 
-def rename_one_by_select(soup: BeautifulSoup, selector: str, replace_name: str) -> None:
+def rename_one_by_select(soup: BeautifulSoup | Tag, selector: str, replace_name: str) -> None:
+    """Меняет имя одному HTML-тегу на заданное по селектору для `soup.select_one`.
+
+    Args:
+        soup (BeautifulSoup | Tag): Объект beautiful soup.
+        selector (str): CSS-селектор для `soup.select_one`
+        replace_name (str): Имя для замены.
+    """
     tag = soup.select_one(selector)
-    _raname_element(tag, replace_name=replace_name)
+    _rename_element(tag, replace_name=replace_name)
 
 
 # TODO: Это работает с html5lib, но релевантно ли это?
-def convert_tables_to_divs(soup: BeautifulSoup):
+def convert_tables_to_divs(soup: BeautifulSoup | Tag):
+    """Преобразует все таблицы в HTML-коде в `<div>` блоки.
+    Это полезно, если на сайте присутсвтвует табличная вёрстка, которая портит структуру HTML-кода.
+
+    Args:
+        soup (BeautifulSoup | Tag): Объект beautiful soup.
+    """
     rename_all_by_select(soup, "table", replace_name="div")
 
 
-def _raname_element(tag: Tag | None, replace_name: str) -> None:
+def _rename_element(tag: Tag | None, replace_name: str) -> None:
     if tag is None:
         return
 
