2a17f9815991d11d556a621f6a7f9d32f7295c41
LEv145 <you@example.com>
2024-09-30 09:30:08 +0200

    Убраны лишние пробелы

--- examples/refiners/restructure_tables.py
+++ examples/refiners/restructure_tables.py
@@ -5,8 +5,6 @@ from ai_assistant_parsers_core.parsers.utils.clean_blocks import clean_all_by_se
 from ai_assistant_parsers_core.parsers.utils.restructure_blocks import rename_all_by_select
 
 
-
-
 class RestructureTablesParsingRefiner(ABCParsingRefiner):
     def refine(self, soup: BeautifulSoup) -> None:
         clean_all_by_select(soup, ".table__col-title")  # Имеет `display: none;`
