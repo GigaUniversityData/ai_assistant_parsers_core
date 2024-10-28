e0e1b10c1ccccdb3f72067f40c44d3381f80ea48
LEv145 <you@example.com>
2024-10-28 18:59:42 +0200

    Переработаны `fetcher`'ы Изменена структура `fetcher` - теперь они создают объект сессии только в методе `open`, а не требуют готовую сессию Теперь `AiohttpFetcher` более точно пытается определить кодировки и не делает новые запросы на сервер в случае неудачи

--- src/ai_assistant_parsers_core/fetchers/impls/aiohttp.py
+++ src/ai_assistant_parsers_core/fetchers/impls/aiohttp.py
@@ -1,21 +1,39 @@
+import typing as t
+from contextlib import suppress
+
+import charset_normalizer
 from aiohttp import ClientSession
 
 from ..abc import ABCFetcher
 
 
 class AiohttpFetcher(ABCFetcher):
-    def __init__(self, client: ClientSession) -> None:
-        self._client = client
+    def __init__(self, client_arguments: dict[str, t.Any] | None = None) -> None:
+        if client_arguments is None:
+            client_arguments = {}
+
+        self._client: ClientSession | None = None
+        self._client_arguments = client_arguments
 
     async def open(self) -> None:
-        pass
+        self._client = ClientSession(**self._client_arguments)
 
     async def fetch(self, url: str) -> str:
         async with self._client.get(url) as response:
-            try:
-                return await response.text()
-            except UnicodeDecodeError:
-                return await response.text(encoding="windows-1251")
+            byte_string = await response.read()
+
+            with suppress(UnicodeDecodeError):
+                encoding = response.get_encoding()
+                return byte_string.decode(encoding=encoding)
+
+            with suppress(UnicodeDecodeError):
+                return byte_string.decode(encoding="windows-1251")
+
+            result = charset_normalizer.detect(byte_string)
+            if result["encoding"] is not None:
+                return byte_string.decode(encoding=result["encoding"])
+
+            raise RuntimeError("The encoding could not be detected automatically") from None
 
     async def close(self) -> None:
         await self._client.close()
--- src/ai_assistant_parsers_core/fetchers/impls/selenium.py
+++ src/ai_assistant_parsers_core/fetchers/impls/selenium.py
@@ -1,14 +1,25 @@
+import typing as t
+
 from selenium.webdriver.remote.webdriver import WebDriver
 
 from ..abc import ABCFetcher
 
 
 class SeleniumFetcher(ABCFetcher):
-    def __init__(self, webdriver: WebDriver) -> None:
-        self._webdriver = webdriver
+    def __init__(
+        self,
+        webdriver_class: type[WebDriver],
+        webdriver_arguments: dict[str, t.Any] | None = None,
+    ) -> None:
+        if webdriver_arguments is None:
+            webdriver_arguments = {}
+
+        self._webdriver: WebDriver | None = None
+        self._webdriver_class = webdriver_class
+        self._webdriver_arguments = webdriver_arguments
 
     async def open(self) -> None:
-        pass
+        self._webdriver = self._webdriver_class(**self._webdriver_arguments)
 
     async def fetch(self, url: str) -> str:
         self._webdriver.get(url)
