abb7a6e9c04622c6630c767006225730812256be
LEv145 <you@example.com>
2024-11-29 19:31:34 +0200

    Обновлён `AiohttpFetcher` для работы со специфичными сайтами - Добавлена система Retry - Добавлен retry без `ssl`

--- src/ai_assistant_parsers_core/fetchers/impls/aiohttp.py
+++ src/ai_assistant_parsers_core/fetchers/impls/aiohttp.py
@@ -4,7 +4,8 @@ import typing as t
 from contextlib import suppress
 
 import charset_normalizer
-from aiohttp import ClientSession
+from aiohttp import ClientSession, ConnectionTimeoutError
+from aiohttp_retry import RetryClient, ExponentialRetry
 
 from ..abc import ABCFetcher
 
@@ -17,38 +18,55 @@ class AiohttpFetcher(ABCFetcher):
             client_arguments = {}
 
         self._client: ClientSession | None = None
+        self._retry_client: RetryClient | None = None
         self._client_arguments = client_arguments
 
     async def open(self) -> None:
         """Открывает фетчер."""
         self._client = ClientSession(**self._client_arguments)
+        self._retry_client = RetryClient(
+            client_session=self._client,
+            retry_options=ExponentialRetry(
+                attempts=5,
+                start_timeout=0.5,
+                max_timeout=30.0,
+                factor=2.0,
+                statuses={500, 502, 503, 504, 429},
+            ),
+        )
 
     async def fetch(self, url: str) -> str:
         """Извлекает HTML из URL-адреса."""
         if not self.is_open():
             raise RuntimeError("Fetcher is not open")
 
-        async with self._client.get(url) as response:
-            byte_string = await response.read()
-
-            with suppress(UnicodeDecodeError):
+        try:
+            async with self._retry_client.get(url) as response:
+                byte_string = await response.read()
                 encoding = response.get_encoding()
-                return byte_string.decode(encoding=encoding)
+        except ConnectionTimeoutError:
+            async with self._retry_client.get(url, ssl=False) as response:
+                byte_string = await response.read()
+                encoding = response.get_encoding()
 
-            with suppress(UnicodeDecodeError):
-                return byte_string.decode(encoding="windows-1251")
+        with suppress(UnicodeDecodeError):
+            return byte_string.decode(encoding=encoding)
 
-            result = charset_normalizer.detect(byte_string)
-            if result["encoding"] is not None:
-                return byte_string.decode(encoding=result["encoding"])
+        with suppress(UnicodeDecodeError):
+            return byte_string.decode(encoding="windows-1251")
 
-            raise RuntimeError("The encoding could not be detected automatically") from None
+        result = charset_normalizer.detect(byte_string)
+        if result["encoding"] is not None:
+            return byte_string.decode(encoding=result["encoding"])
+
+        raise RuntimeError("The encoding could not be detected automatically") from None
 
     async def close(self) -> None:
         """Закрывает фетчер."""
         await self._client.close()
         self._client = None
+        self._retry_client = None
 
     def is_open(self) -> bool:
         """Проверяет открыт ли фетчер."""
-        return self._client is not None
+        return self._client is not None and self._retry_client is not None
