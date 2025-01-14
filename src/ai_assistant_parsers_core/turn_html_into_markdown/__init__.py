1dd712e6b2397616505dfa4cf50c35be9ae40c5b
LEv145 <you@example.com>
2025-01-14 18:34:34 +0200

    TYPO

--- src/ai_assistant_parsers_core/turn_html_into_markdown/__init__.py
+++ src/ai_assistant_parsers_core/turn_html_into_markdown/__init__.py
@@ -7,7 +7,8 @@ SERVER_URL = "http://localhost:8080"
 
 API_URL = getenv("AAPC__MARKDOWN_API_URL", "http://localhost:8080")
 
-async def turn_html_into_markdown(html: str,) -> str:
+
+async def turn_html_into_markdown(html: str) -> str:
     """Преобразовывает HTML в Markdown.
 
     Args:
